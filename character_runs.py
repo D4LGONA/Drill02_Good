07d083a812b78c1ba2357f908252ccb4e13965d4
D4LGONA(samsung) <laff3y2297@gmail.com>
2023-09-20 17:43:24 +0900

    전체 뼈대 완성

--- character_runs.py
+++ character_runs.py
@@ -1,11 +1,23 @@
 from pico2d import *
+import math
 
 open_canvas()
 
-# fill here
 grass = load_image('grass.png')
 character = load_image('character.png')
 
+def run_circle():       # pass 일단 써놓기
+    print('circle')
+    pass
+
+def run_rectangle():
+    print('rectangle')
+    pass
+
+while True:
+    run_circle()        # 함수를 먼저 만들고 내용을 만들 것
+    run_rectangle()     
+    break               # break를 써서 우선 제대로 되는지 확인
 
 
 close_canvas()
